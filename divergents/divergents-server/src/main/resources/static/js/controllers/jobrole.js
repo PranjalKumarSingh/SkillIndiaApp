23fb42b662b92e079eab1bff3971d2aa452fe01e
10pratibhajha <40226742+10pratibhajha@users.noreply.github.com>
2018-07-16 17:03:35 +0530

    author:Pratibha Jha file updated:jobrole.js,trainingMaterial.js description:updated js files

--- divergents/divergents-server/src/main/resources/static/js/controllers/jobrole.js
+++ divergents/divergents-server/src/main/resources/static/js/controllers/jobrole.js
@@ -1,5 +1,95 @@
 var app = angular.module('skillIndiaApp');
-app.controller('jobRoleController', function($scope){
-	
-});
-	
+
+app.controller('jobRoleController', function($scope,$http){
+      $scope.gridJobRoles = {
+        enableGridMenus: false,
+        enableSorting: false,
+        enableFiltering: false,
+        enableCellEdit: false,
+        enableColumnMenus: false,
+        enableHorizontalScrollbar: 0,
+        enableVerticalScrollbar: 0,
+        paginationPageSizes: [5, 10, 20, 30],
+        paginationPageSize: 10,
+        useExternalPagination: true,
+
+        columnDefs: [
+            {
+            	name: 'jobRoleId', 
+                displayName: 'Id'
+            },
+            {
+            	name: 'jobRole',
+            	displayName: 'Job Role'
+            },
+            {
+            	name: 'sectorSkillCouncil', 
+                displayName: 'Sector Skill Council'
+            },
+            {
+            	 name: 'Trainingcenter', 
+            	 displayName: 'Source(Online|Material)', 
+            	 cellTemplate: '<a ng-href="{{row.entity.Trainingcenter}}"><button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#loginModal" ng-click="grid.appScope.getTrainingDetails()">Check Training Material</button></a>'
+             }
+        ]
+    }
+   
+   
+    $scope.gridTrainingCenterDetails = {
+            enableGridMenus: false,
+            enableSorting: false,
+            enableFiltering: false,
+            enableCellEdit: false,
+            enableColumnMenus: false,
+            enableHorizontalScrollbar: 0,
+            enableVerticalScrollbar: 0,
+            paginationPageSizes: [5, 10, 20, 30],
+            paginationPageSize: 10,
+            useExternalPagination: true,
+
+            columnDefs: [
+                {
+                	name: 'trainingCenterName', 
+                    displayName: 'Training Center'
+                },
+                {
+                	name: 'states',
+                	displayName: 'States'
+                }
+            ]
+        };
+     
+    $scope.getTrainingDetails=function(){
+    	
+    	console.log("button is working");
+    	$http.get('')
+        .then(function (response) {
+        	 $scope.gridTrainingCenterDetails.data= response.data;
+
+        });
+    };
+    
+    $scope.getTableHeight=function(){
+    	var rowHeight=30;
+    	var headerHeight=30;
+    	return{
+    		height: ($scope.details.data.length * rowHeight + headerHeight) + "px"
+    		
+    	};
+    };
+	$scope.x="";
+	$scope.search = function(alphabet){
+	console.log("Success and the filter is " + alphabet);
+	$http.get('/getJobRoleForSelectedLetter?letter='+alphabet);
+   };
+    
+    
+    $http.get('/')
+    .then(function (response) {
+    	 $scope.gridJobRoles.data= response.data;
+
+    });
+    
+    });
+
+
--- divergents/divergents-server/src/main/resources/static/js/controllers/trainingMaterial.js
+++ divergents/divergents-server/src/main/resources/static/js/controllers/trainingMaterial.js
@@ -1,5 +1,55 @@
 var app = angular.module('skillIndiaApp');
-app.controller('trainingMaterialController', function($scope){
+app.controller('trainingMaterialController', function($scope,$http){
 	
+       $scope.gridTraining = {
+        enableGridMenus: false,
+        enableSorting: false,
+        enableFiltering: false,
+        enableCellEdit: false,
+        enableColumnMenus: false,
+        enableHorizontalScrollbar: 0,
+        enableVerticalScrollbar: 0,
+        paginationPageSizes: [5, 10, 20, 30],
+        paginationPageSize: 10,
+        useExternalPagination: true,
+
+        columnDefs: [
+            {
+            	name: 'trainingMaterialId', 
+                displayName: 'Training Material Id'
+            },
+            {
+            	name: 'trainingTitle',
+            	displayName: 'Training Title'
+            },
+            
+            {
+            	 name: 'sources', 
+            	 displayName: 'Source(Online|Material)', 
+            	 cellTemplate: '<a ng-href="{{row.entity.sources}}" target="_blank" download><img src="images/pdf.png" class="pointer"></a>'
+             }
+        ]
+    };
+    
+    $scope.getTableHeight=function(){
+    	var rowHeight=30;
+    	var headerHeight=30;
+    	return{
+    		height:($scope.gridTraining.data.length * rowHeight + headerHeight) + "px"
+    	};
+    };
+    
+	    
+    $http.get('/staticJson/trainingmaterial.json')
+    .then(function (response) {
+    	console.log("Working ui grid");
+    	 $scope.gridTraining.data= response.data;
+
+    });
+    
+    
+   
+ 
+     
 });
-	
+
