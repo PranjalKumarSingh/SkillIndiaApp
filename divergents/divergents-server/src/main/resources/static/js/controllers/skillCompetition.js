a7f4a72dee72fdfc6ef128bd07e59486294e64c8
sumitkumar1103 <40226861+sumitkumar1103@users.noreply.github.com>
2018-07-17 16:44:27 +0530

    author:shweta file added:skillCompetition.js

--- divergents/divergents-server/src/main/resources/static/js/controllers/skillCompetition.js
+++ divergents/divergents-server/src/main/resources/static/js/controllers/skillCompetition.js
@@ -1,5 +1,38 @@
 var app = angular.module('skillIndiaApp');
 app.controller('skillCompetitionController', function($scope){
+	 $scope.sectors =[
+	              	
+	              	
+	                  {
+	                    
+	                    'id': 'Creative Arts and Fashion',
+	                     'name': "Creative Arts and Fashion",
+	                     'categories':  [{
+	                        'id': 'Fashion Technology',
+	                        'dep': "Fashion Technology"
+	                    }, {
+	                        'id': 'Floristry',
+	                        'dep': "Floristry"
+	                    },
+	                    {
+	                        'id': 'Graphic Design Technology',
+	                        'dep': "Graphic Design Technology"
+	                   
+	                    
+	                    
+	                    },
+	                    {
+	                        'id': 'jewellery',
+	                        'dep': "jewellery"
+	                   
+	                   
+	                    
+	                    
+
+	                    },
+	                    
+	                    ]
+	                  }]
 	
 });
 	
