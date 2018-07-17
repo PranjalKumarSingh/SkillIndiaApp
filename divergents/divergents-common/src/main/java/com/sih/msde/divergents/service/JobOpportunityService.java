ba08ed7b715ef9c7a90ec53efa94e2f2fa0ae846
AshishVerma4 <40226678+AshishVerma4@users.noreply.github.com>
2018-07-17 19:07:52 +0530

    Author : Ashish Verma Files Added: JobOpportunityService.java Description : Service file of Partial Job opportunity

---
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/service/JobOpportunityService.java
@@ -0,0 +1,36 @@
+package com.sih.msde.divergents.service;
+
+import java.util.Collection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.sih.msde.divergents.dao.JobOpportunityDao;
+import com.sih.msde.divergents.dto.JobOpportunityDto;
+
+
+@Service
+public class JobOpportunityService {
+@Autowired
+private JobOpportunityDao jobOpportunityDao;
+
+private static final Logger LOGGER= LoggerFactory.getLogger(JobOpportunityService.class);
+public Collection<JobOpportunityDto> getJobOpportunity(){
+	
+	LOGGER.debug("Request received from Controller");
+	LOGGER.debug("In Job opportunity Service, to get Job opportunity details");
+	
+	try {
+		LOGGER.debug("In try block to get Job opportunity details");
+		return jobOpportunityDao.getJobOpportunity();
+	}
+	catch (Exception e) {
+	
+		LOGGER.debug("An error occurred while getting the details of job opportunity : " + e);
+		return null;
+	}
+}
+}
