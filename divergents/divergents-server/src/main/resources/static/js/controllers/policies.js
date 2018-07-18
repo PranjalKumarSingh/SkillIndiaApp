30b31d1281ebf792e5526447067ed11dae8e5a56
mahekgupta17 <40226383+mahekgupta@users.noreply.github.com>
2018-07-18 07:06:57 +0530

    Author: Mahek Gupta file added: policies.js Description: updated the files.

--- divergents/divergents-server/src/main/resources/static/js/controllers/policies.js
+++ divergents/divergents-server/src/main/resources/static/js/controllers/policies.js
@@ -1,46 +1,48 @@
 var app = angular.module('skillIndiaApp');
 app.controller('policiesController', function($scope,$http){
 	
-	
+
 	$scope.details = {
-		        enableGridMenus: false,
-		        enableSorting: false,
-		        enableFiltering: false,
-		        enableCellEdit: false,
-		        enableColumnMenus: false,
-		        enableHorizontalScrollbar: 0,
-		        enableVerticalScrollbar: 0,
-		        paginationPageSizes: [5, 10, 20, 30],
-		        paginationPageSize: 10,
-		        useExternalPagination: true,
+	        enableGridMenus: false,
+	        enableSorting: false,
+	        enableFiltering: false,
+	        enableCellEdit: false,
+	        enableColumnMenus: false,
+	        enableHorizontalScrollbar: 0,
+	        enableVerticalScrollbar: 0,
+	        paginationPageSizes: [5, 10, 20, 30],
+	        paginationPageSize: 10,
+	        useExternalPagination: true,
 
-		        columnDefs: [
-		            {
-		            	name: 'policies reports', 
-		                displayName: 'Policies|Reports'
-		            },
-		            {
-		            	name: 'title',
-		            	displayName: 'Title'
-		            },
-		            
-					 {
-		                name: 'description',
-		                displayName: 'Description',
-		               cellTemplate: '<a ng-href="{{row.entity.description}}" target="_blank" download><img src="images/pdf.png" class="pointer"></a>'
-		               }
-		             
-		        ]
-		    };
-		   
-		    $http.get('/json/policies.json')
-		    .then(function (response) {
-		    	 $scope.details.data= response.data;
-		    
-		    });  
-		   
-		  
-		    
-		});
+	        columnDefs: [
+	            {
+	            	name: 'policies reports', 
+	                displayName: 'Policies|Reports'
+	            },
+	            {
+	            	name: 'title',
+	            	displayName: 'Title'
+	            },
+	            
+				 {
+	                name: 'description',
+	                displayName: 'Description',
+	               cellTemplate: '<a ng-href="{{row.entity.description}}" target="_blank" download><img src="images/pdf.png" class="pointer"></a>'
+	               }
+	             
+	        ]
+	    };
+	   
+	    $http.get('/json/policies.json')
+	    .then(function (response) {
+	    	 $scope.details.data= response.data;
+	    
+	    });  
+	   
+	  
+	    
+	});
 
 	
+	
+
