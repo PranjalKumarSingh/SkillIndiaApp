20f6fa2e7c934ca3c8970f4c8b326b95132ee7f7
poojasharma06 <sharmapooja6797@gmail.com>
2018-07-19 22:58:11 +0530

    Author:  Pooja Sharma Files Modified: webinar.css, webinar.html, webinar.js Files Added: data1.json, data2.json

--- divergents/divergents-server/src/main/resources/static/js/controllers/webinar.js
+++ divergents/divergents-server/src/main/resources/static/js/controllers/webinar.js
@@ -1,58 +1,92 @@
-var app = angular.module('skillindia', ['ngRoute','ui.grid',
-	                                        'ui.grid.edit',
-	                                        'ui.grid.cellNav',
-	                                        'ui.grid.autoResize',
-	                                        'ui.bootstrap',
-	                                        'ui.grid.pagination']);
+app.controller('webinar' , function($scope,$http){
+	
+	$scope.showWebinar= false;
+	$scope.errorMessage="";
+	
+	
+$scope.gridOptionwebinar = {
+	         enableGridMenus : false, 
+	         rowHeight: 30,
+	         enableSorting: false, 
+	         enableFiltering: false,
+	         enableCellEdit : false,
+	         enableColumnMenus : false,
+	         enableHorizontalScrollbar:0,
+	         enableVerticalScrollbar:0,
+	         paginationPageSizes: [10, 2, 50],
+	         paginationPageSize: 2,   
+	         useExternalPagination: true,  
+	         
+	         columnDefs: [
+	              { name: 'id', displayName: 'Id', width:"5%"},
+	              { name: 'title', displayName: 'Title' },
+	              { name: 'category',displayName: 'Category'},
+	              { name: 'partner',displayName: 'Author'},
+	              { name: 'schedule',displayName: 'Schedule' },
+	              { name: 'register',displayName: 'Register',cellTemplate:'<a><img src="images/tick.png" class="pointer"></a>'}
+	              ]
+	  }; 	
+	
+	
 
-	app.controller('webinarController',function($scope,$http) {
-	    $scope.details = {
-	        enableGridMenus: false,
-	        enableSorting: false,
-	        enableFiltering: false,
-	        enableCellEdit: false,
-	        enableColumnMenus: false,
-	        enableHorizontalScrollbar: 0,
-	        enableVerticalScrollbar: 0,
-	        paginationPageSizes: [5, 10, 20, 30],
-	        paginationPageSize: 10,
-	        useExternalPagination: true,
+	$http.get('jsonData/data1.json')
+    .success(function (response) {
+        $scope.gridOptionwebinar.data= response;
+    })
+    .error(function (error) {
+       console.log("Error"+ error);
+    });
+	
+	
+	$scope.showWebinarFunc=function(){
+		if ($scope.searchtitle== ""){
+			$scope.errorMessage="Enter title to search";
+		}
+		else{
+			$scope.errorMessage="";
+			$scope.showWebinar= true;
+		}
+	}
+	
+	$scope.gridOptionwebinar2 = {
+	         enableGridMenus : false,  
+	         enableSorting: false, 
+	         enableFiltering: false,
+	         enableCellEdit : false,
+	         enableColumnMenus : false,
+	         enableHorizontalScrollbar:0,
+	         enableVerticalScrollbar:0,
+	         paginationPageSizes: [10, 2, 50],
+	         paginationPageSize: 2,   
+	         useExternalPagination: true,  
+	         
+	         columnDefs: [
+	              { name: 'id', displayName: 'Id', width:"5%"},
+	              { name: 'title', displayName: 'Title' },
+	              { name: 'category',displayName: 'Category'},
+	              { name: 'partner',displayName: 'Author'},
+	              { name: 'schedule',displayName: 'Schedule' },
+	              { name: 'register',displayName: 'Register',cellTemplate:'<a><img src="images/tick.png" class="pointer"></a>'}
+	              ]
+	  }; 	
+	
+	
 
-	        columnDefs: [
-	            {
-	            	name: 'id', 
-	                displayName: 'ID'
-	            },
-	            {
-	            	name: 'title',
-	            	displayName: 'Title'
-	            },
-	            {
-	                name: 'category',
-	                displayName: 'Category'
-	            },
-	            {
-	                name: 'author',
-	                displayName: 'Author'
-	            },
-	            {
-	                name: 'schedule',
-	                displayName: 'Schedule'
-	             },
-				 {
-	                name: 'register',
-	                displayName: 'Register',
-	                cellTemplate: '<i class="glyphicon glyphicon-ok"></i>'
-	             }       
-	        ]
+	$http.get('jsonData/data2.json')
+   .success(function (response) {
+       $scope.gridOptionwebinar2.data= response;
+   })
+   .error(function (error) {
+      console.log("Error"+ error);
+   });
+	
+	$scope.getTableHeight = function() {
+	       var rowHeight = 30;
+	       var headerHeight = 30;
+	       return {
+	          height: ($scope.gridOptionwebinar.data.length * rowHeight + headerHeight) + "px"
+	       };
 	    };
-	    $http.get('/')
-	    .then(function (response) {
-	    	
-	    	 $scope.details.data= response.data;
-
-	    });     
-	});
+	      
 
 });
-	
