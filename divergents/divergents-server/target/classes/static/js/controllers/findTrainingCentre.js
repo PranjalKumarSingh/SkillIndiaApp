eb5533d9015ea4d1ea34fc5ef00f6a0d2c4c517d
10pratibhajha <40226742+10pratibhajha@users.noreply.github.com>
2018-07-19 23:19:41 +0530

    Author : Pratibha Jha Files Modified : findTrainingCentre.js Description :  modified

--- divergents/divergents-server/target/classes/static/js/controllers/findTrainingCentre.js
+++ divergents/divergents-server/target/classes/static/js/controllers/findTrainingCentre.js
@@ -1,5 +1,57 @@
 var app = angular.module('skillIndiaApp');
-app.controller('findTCController', function($scope){
+app.controller('findTCController', function($scope,$http){
+	$scope.details = {
+			 enableGridMenus: false,
+		        enableSorting: false,
+		        enableFiltering: false,
+		        enableCellEdit: false,
+		        enableColumnMenus: false,
+		        enableHorizontalScrollbar: 0,
+		        enableVerticalScrollbar: 0,
+		        paginationPageSizes: [5, 10, 20, 30],
+		        paginationPageSize: 10,
+		        useExternalPagination: true,
+
+	        columnDefs: [
+
+	   	              { name: 'trainingCenterName', displayName: 'Center Name', cellClass:'',headerCellClass:''},
+	   	              { name: 'address', displayName: 'Address' ,cellClass:'',headerCellClass:'' },
+	   	              { name: 'contactNumber',displayName: 'Contact' , cellClass:'',headerCellClass:''},
+	   	              { name: 'Interest', displayName:'Show Interest' , cellTemplate: '<a ng-href="{{row.entity.Trainingcenter}}"><i class="glyphicon glyphicon-thumbs-up" style="cursor: pointer;" ng-click=grid.appScope.showYourInterest()></i></a>',headerCellClass:'trainingCenters',cellClass:'trainingCenterCellClass'}
+	   	           
+	   	   ]
+	           
+	             
+	  };
 	
+	$scope.IsVisible = false;
+
+    $scope.ShowHide = function(){
+        $scope.IsVisible = $scope.IsVisible = true;
+    }
+	    
+	    
+	    
+	   
+	    $http.get('/jsonData/findCenter.json')
+	    .then(function (response) {
+	    	 $scope.details.data= response.data;
+
 });
-	
+	    
+	    
+	    $scope.showYourInterest=function(){
+			if($scope.showInterest== false){
+			$scope.showInterest= true;}
+			else{
+				$scope.showInterest= false;
+			}
+			
+			
+		}
+});    
+
+    
+ 
+   
+ 	
