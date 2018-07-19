5abb8bd78c322b78166aa24ae55918a6deab9f49
AshishVerma4 <40226678+AshishVerma4@users.noreply.github.com>
2018-07-19 10:39:56 +0530

    Author : Ashish Verma Files Modified : JobOpportunityController.java Description : Modified file of Controller

--- divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/JobOpportunityController.java
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/JobOpportunityController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sih.msde.divergents.dto.JobOpportunityDto;
@@ -20,12 +21,14 @@ public class JobOpportunityController {
 	private static final Logger LOGGER= LoggerFactory.getLogger(JobOpportunityController.class);
 	
 	@RequestMapping("/showJobOpportunity")
-	public Collection<JobOpportunityDto> getJobOpportunity(){
+	public Collection<JobOpportunityDto> getJobOpportunity(@RequestParam("location") String location){
 		
 		 	
 		try {
-			
-			return jobOpportunityService.getJobOpportunity();
+			LOGGER.error("in try block of controller");
+			LOGGER.error("Sending request to service :" +location);
+			Collection<JobOpportunityDto> oldCollection = jobOpportunityService.getJobOpportunity(location);
+			return oldCollection;
 			
 		}catch(Exception e) {
 
@@ -36,4 +39,24 @@ public class JobOpportunityController {
 	
 
 }
+	
+	@RequestMapping("/populategrid")
+	public Collection<JobOpportunityDto> getPopulatedetailsGrid(){
+		
+	 	
+		try {
+			LOGGER.error("in try block of controller");
+			LOGGER.error("Sending request to service :");
+			return jobOpportunityService.getPopulateDetailsGrid();
+			
+		}catch(Exception e) {
+
+			LOGGER.debug("The exception is : " + e);
+			return null;
+			
+		}
+	
+
+}
+	
 }
