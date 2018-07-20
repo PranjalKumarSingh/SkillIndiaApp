0cd9d45020ebe0b7e22f148150b4f786deb3ee1b
abesparnita <40226683+abesparnita@users.noreply.github.com>
2018-07-20 09:35:30 +0530

    Author:Parnita file added:TalentAquisitionController.java

--- divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/TalentAquisitionController.java
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/TalentAquisitionController.java
@@ -5,7 +5,9 @@ import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +26,7 @@ public class TalentAquisitionController {
 	private TalentAquisitionService talentAquisitionService;
 	
 	
-	@RequestMapping("/getCandidateDetails")
+	@RequestMapping(value = "/getCandidateDetails", method=RequestMethod.GET)
 	public Collection<TalentAquisitionDto> getCandidateDetails(@RequestParam("jobRole") String jobRole){
 		
 		 	
